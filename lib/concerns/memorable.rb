59c3e03f758b34cd6c0e03b7cc063be98a6adef3
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-29 05:03:25 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,32 @@
+
+
+module Memorable
+
+  module ClassMethods #ClassMethods are extend
+    def reset_all
+    self.all.clear
+    end
+
+    def count
+    self.all.count
+    end
+
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
+
+  end
+
+  module InstanceMethods #InstanceMethods are include
+    def initialize
+      self.class.all << self
+    end
+
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+
+
+
+end
