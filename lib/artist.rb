47247c8e81cdf3a26cc90a8de5faf1a2d55f866d
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-29 04:26:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
@@ -28,7 +28,7 @@ class Artist
   # def self.reset_all
   #   self.all.clear
   # end
-  
+
   #
   # def self.count
   #   self.all.count
